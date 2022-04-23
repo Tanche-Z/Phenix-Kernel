66a1f17e644bf71c9e7c175915f4ed7d9e4dd536
Huayu Zhang <zhanghuayu1233@gmail.com>
2022-04-23 21:11:43 +0100

    Protected Mode and GDT

--- src/loader.asm
+++ src/loader.asm
@@ -5,7 +5,7 @@ dw 0x55aa ;magic for judgement
 mov si, loading
 call print
 
-xchg bx, bx ;break point
+; xchg bx, bx ;break point
 
 detect_memory:
     ;set ebx to 0
@@ -43,21 +43,23 @@ detect_memory:
     call print
 
     xchg bx, bx
+    mov byte [0xb8000], 'P'
 
-    ; the count of structural body
-    mov cx, [ards_count]
-    ; the pointer of structural body
-    mov si, 0
-.show:
-    mov eax, [ards_buffer + si]
-    mov ebx, [ards_buffer + si + 8]
-    mov edx, [ards_buffer + si + 16]
-    add si, 20
+    jmp pre_protected_mode
+
+pre_protected_mode:
     xchg bx, bx
-    loop .show
-
-jmp $
-
+    cli ; close interrupt
+    ; open A20
+    in al, 0x92
+    or al, 0b10
+    out 0x92, al
+    lgdt [gdt_ptr]; load get
+    ; PE (Protect Enable)
+    mov eax ,cr0
+    or eax, 1
+    mov cr0 , eax
+    jmp dword code_selector:protect_mode ; use jmp to refresh cache
 
 print:
     mov ah, 0x0e
@@ -83,6 +85,51 @@ error:
     hlt
     jmp $
     .msg db "Loading Error!!!", 10, 13,0
+
+[bits 32]
+protect_mode:
+    xchg bx, bx
+    mov ax, data_selector
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax ;initialize segment register
+
+    mov sp, 0x10000 ; modify stack top
+    mov byte [0xb8000], 'P'
+    mov byte [0x200000], 'P'
+
+jmp $
+
+code_selector equ (1 << 3)
+data_selector equ (2 << 3)
+
+memory_base equ 0 ; base address: where the memory start
+memory_limit equ ((1024* 1024* 1024 * 4) / (1024 * 4)) -1
+
+gdt_ptr:
+    dw (gdt_end - gdt_base) - 1
+    dd gdt_base
+gdt_base:
+    dd 0, 0 ; NULL Discriptor
+gdt_code:
+    dw memory_limit & 0xffff ; segment limit 0~15
+    dw memory_base & 0xffff ; base address 0~16
+    db (memory_base >> 16) & 0xff; base address 16~24
+    db 0b_1_00_1_1_0_1_0 ; exist_dpl(discriptor privilege level) is 0_S is Code_ NOT compliance_writable_haven't been accessed
+    db 0b1_1_0_0_0000 | (memory_limit >> 16) & 0xf ; 4K_32bit_not 64bit_available to OS_segment limit 16~19
+    db (memory_base >> 24) & 0xff ; base address 24~31
+
+gdt_data:
+    dw memory_limit & 0xffff ; segment limit 0~15
+    dw memory_base & 0xffff ; base address 0~16
+    db (memory_base >> 16) & 0xff; base address 16~24
+    db 0b_1_00_1_0_0_1_0 ; exist_dpl(discriptor privilege level) is 0_S is Data_upward extend_writable_haven't been accessed
+    db 0b1_1_0_0_0000 | (memory_limit >> 16) & 0xf ; 4K_32bit_not 64bit_available to OS_segment limit 16~19
+    db (memory_base >> 24) & 0xff ; base address 24~31
+
+gdt_end:
 
 ards_count:
     dw 0
