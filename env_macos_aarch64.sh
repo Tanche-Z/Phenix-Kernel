ec76f6014e9966d6665fd258e34041e1b856b87a
Huayu Zhang <zhanghuayu.dev@gmail.com>
2022-09-15 13:57:32 +0100

    add env variable setting script for macos(darwin) arm64

---
+++ env_macos_aarch64.sh
@@ -0,0 +1,8 @@
+# run in shell: `. ./env_macos_aarch64.sh` or `source ./env_macos_aarch64.sh`
+export NASM=/opt/homebrew/bin/nasm
+export AS=/opt/homebrew/bin/i686-elf-as
+export CC=/opt/homebrew/bin/i686-elf-gcc
+export LD=/opt/homebrew/bin/i686-elf-ld
+export OBJCOPY=/opt/homebrew/bin/i686-elf-objcopy
+export OBJDUMP=/opt/homebrew/bin/i686-elf-objdump
+export GDB=/opt/homebrew/bin/i386-elf-gdb
