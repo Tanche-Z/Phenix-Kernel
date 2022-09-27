409ee4b3685bee972ed9fc6cd9e6bf4c4f5db6d2
Huayu Zhang <zhanghuayu.dev@gmail.com>
2022-09-27 22:19:35 +0000

    add kali x86_64 env

---
+++ devel_env_scripts/env_kali_linux_x86_64.sh
@@ -0,0 +1,8 @@
+# run in shell: `. ./env_kali_linux_x86_64.sh` or `source ./env_kali_linux_x86_64.sh`
+export NASM=/usr/bin/nasm
+export AS=/usr/bin/i686-linux-gnu-as
+export CC=/usr/bin/i686-linux-gnu-gcc-12
+export LD=/usr/bin/i686-linux-gnu-ld
+export OBJCOPY=/usr/bin/i686-linux-gnu-objcopy
+export OBJDUMP=/usr/bin/i686-linux-gnu-objdump
+export GDB=/usr/bin/gdb
